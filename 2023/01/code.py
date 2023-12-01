d9ce70d7cb1aa2a3cd202ba4a99939e49624e4b3
sheeperino <sheeperinosophie@gmail.com>
2023-12-01 15:53:48 +0100

    2023.1.2

--- 2023/01/code.py
+++ 2023/01/code.py
@@ -1,12 +1,34 @@
 # https://adventofcode.com/2023/day/1
 
-with open("input") as f:
+def part_1(lines):
     calibration_values = []
-    for line in f:
+    for line in lines:
         digits_only = list(filter(lambda x: x.isdigit(), line))
         calibration = int(digits_only[0] + digits_only[-1])
         calibration_values.append(calibration)
 
-    answer_1 = sum(calibration_values)
-    print(answer_1)
+    return sum(calibration_values)
 
+def part_2(lines):
+    calibration_values = []
+    allowed_digits = {
+        "one": "o1e", "two": "t2o", "three": "t3e", "four": "f4r",
+        "five": "f5e", "six": "s6x", "seven": "s7n", "eight": "e8t", "nine": "n9e"
+    }
+
+    for line in lines:
+        for k, v in allowed_digits.items():
+            line = line.replace(k, v)
+
+        digits_only = list(filter(lambda x: x.isdigit(), line))
+        calibration = int(digits_only[0] + digits_only[-1])
+        calibration_values.append(calibration)
+
+    return sum(calibration_values)
+
+
+with open("input") as f:
+    lines = f.readlines()
+
+    print("part 1:", part_1(lines))
+    print("part 2:", part_2(lines))
